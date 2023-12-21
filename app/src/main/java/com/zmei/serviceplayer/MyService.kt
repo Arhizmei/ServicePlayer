d61c340e2a44b81adc47d493ba4cd6b5940545d7
Arhizmei <arhizmei@gmail.com>
2023-12-21 14:08:17 +0200

    add metod onDestroy

--- app/src/main/java/com/zmei/serviceplayer/MyService.kt
+++ app/src/main/java/com/zmei/serviceplayer/MyService.kt
@@ -2,6 +2,7 @@ package com.zmei.serviceplayer
 
 import android.content.Intent
 import android.media.MediaPlayer
+import android.provider.Settings
 import android.os.IBinder
 
 class MyService : android.app.Service() {
@@ -9,9 +10,16 @@ class MyService : android.app.Service() {
     private lateinit var player: MediaPlayer
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return super.onStartCommand(intent, flags, startId)
+        player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI)
+        player.setLooping( true)
+        player.start()
+        return START_STICKY
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        player.stop()
+    }
 
     override fun onBind(p0: Intent?): IBinder? {
         return null
