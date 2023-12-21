dcea2841627a8fc4c56ba86179e9ac6c0b42b7a4
Arhizmei <arhizmei@gmail.com>
2023-12-21 14:25:48 +0200

    finish

--- app/src/main/java/com/zmei/serviceplayer/MainActivity.kt
+++ app/src/main/java/com/zmei/serviceplayer/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.zmei.serviceplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zmei.serviceplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            startBtn.setOnClickListener {
+                startService(Intent(this@MainActivity, MyService::class.java))
+            }
+            startBtn.setOnClickListener {
+                stopService(Intent(this@MainActivity, MyService::class.java))
+            }
+        }
     }
 }
